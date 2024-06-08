b7d79220201c7b0d41ab238dfc83c22279e97a76
Xhoani Sadaj <xhoisadaj@gmail.com>
2024-06-08 16:39:02 +0200

    Updated the mongodb connection

--- server.js
+++ server.js
@@ -10,17 +10,18 @@ const PORT = process.env.PORT || 5500;
 app.use(bodyParser.json());
 app.use(cors());
 
-
+// Import routes
 const dogRouter = require('./routes/dog.route');
 const catRouter = require('./routes/cat.route');
 const birdRouter = require('./routes/bird.route');
 
+// Route middleware
 app.use('/dogs', dogRouter);
 app.use('/cats', catRouter);
 app.use('/birds', birdRouter);
 
 // MongoDB Connection
-mongoose.connect('mongodb+srv://root:root@codevider-fullstack.ht2s86s.mongodb.net/?retryWrites=true&w=majority&appName=Codevider-Fullstack', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://root:root@codevider-fullstack.ht2s86s.mongodb.net/?retryWrites=true&w=majority&appName=Codevider-Fullstack');
 const connection = mongoose.connection;
 
 connection.once('open', () => {
